72eddb5ed2c3fe79330a9ed4519b0d1bc843b17b
cemaldogann <ccemal.dogann@gmail.com>
2026-01-11 19:31:27 +0100

    KL_counter and KL_advertise_controller modules introduced

---
+++ hdl/adp/KL_advertise_controller.sv
@@ -0,0 +1,189 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Oguz Kahraman <oguz.kahraman@kebag-logic.com>
+ *
+ * SPDX-License-Identifier: CERN-OHL-W-2.0
+*/
+
+/*
+------------------------------------------------------------------------------
+  File        : KL_advertise_controller.sv
+  Author      : Cemal Dogan
+
+                cemal.dogan@kebag-logic.com
+
+  Date        : 2025-04-21
+  Description : Providing all the necessary events for KL_advertise_state
+                  - Check the rcvd_entity_info for the following fields;
+                    - Entity_id matched (zero or ATDECC entity_id)
+                    - Grandmaster_id
+                  - Start/Stop and generate the events for TMR_DELAY and TMR_ADVERTISE
+  Company     : Kebag Logic
+  Project     : Milan ADP
+
+------------------------------------------------------------------------------
+*/
+
+`default_nettype none
+import adp_pkg::*;
+
+module KL_advertise_controller
+  (
+    input wire clk_i, //! Global clock
+    input wire rst_n, //! Active-low Reset 
+    input wire [63:0] rcv_entity_id_i,  //! entity_info.rcv_entity_id - from KL_adp_parser
+    input wire [63:0] entity_id_i,      //! entity_id of the ATDECC entity - from memory map 
+    input wire [63:0] grandmaster_id_i, //! grandmaster_id - from PTP module
+
+    input wire rcv_adp_discover_i, //! ADP Discovery packet is received and rcv_entity_id_i field is valid- from KL_adp_parser
+    input wire link_down_i, //! Link down event from Upper Management Module (TBD)
+    input wire link_up_i,   //! Link up event from Upper Management Module (TBD)
+    input wire shutdown_i,  //! Shutdown evenet from Upper Management Module (TBD)
+
+    input wire start_tmr_delay_i,     //! Start TMR_DELAY counter command - from KL_advertise_state Module
+    input wire start_tmr_advertise_i, //! Start TMR_ADVERTISE counter command - from KL_advertise_state Module
+    input wire stop_tmr_delay_i,      //! Stop TMR_DELAY counter command from KL_advertise_state Module
+    input wire stop_tmr_advertise_i,  //! Stop TMR_ADVERTISE counter command from KL_advertise_state Module
+
+    output adp_advertise_event_t advertise_event_o //! All Advertise related evenet - to KL_advertise_state
+  );
+  
+  typedef enum bit [1:0] {
+    IDLE_S,
+    CHECK_ENTITY_S,
+    UPDATE_EVENT_S
+  } state_rcv_discover_t;
+
+  state_rcv_discover_t cntrller_state;
+
+  //! RCV_ADP_DISCOVER EVENT related register
+  reg rcv_adp_discover_r;
+
+  //! GM_EVENT related registers
+  reg [63:0] grandmaster_id_r;
+  //! Grandmaster id changed
+  reg gm_change_r;
+
+  //! TMR_DELAY and TMR_ADVERTISE related registers.
+  reg [2:0] zero_four_sec_cnt_r;
+  //! Register that takes the value from 0 to 4.
+  reg [2:0] delay_value_r;
+  //! Start signal for KL_counter.
+  reg start_tmr_delay_cnt_r;
+  //! TMR_ADVERTISE_COUNTER finished counting.
+  reg tmr_advertise_completed_r;
+  //! TMR_DELAY_COUNTER finished counting.
+  reg tmr_delay_completed_r;
+
+
+// ------------ ASYNC ASSIGNMENT ------ ------ //
+  assign advertise_event_o.LINK_DOWN = link_down_i;
+  assign advertise_event_o.LINK_UP = link_up_i;
+  assign advertise_event_o.SHUTDOWN = shutdown_i;
+  assign advertise_event_o.RCV_ADP_DISCOVER = rcv_adp_discover_r;
+  assign advertise_event_o.GM_CHANGE = gm_change_r;
+  assign advertise_event_o.TMR_ADVERTISE = tmr_advertise_completed_r;
+  assign advertise_event_o.TMR_DELAY = tmr_delay_completed_r;
+
+  //! Advertise counter - Static 5 sec delay
+  KL_counter TMR_ADVERTISE_COUNTER (
+    .clk_i         ( clk_i ),
+    .rst_n         ( rst_n ),
+    .start_i       ( start_tmr_advertise_i ),
+    .stop_i        ( stop_tmr_advertise_i ),
+    .delay_value_i ( 3'd5 ),
+    .completed_o   ( tmr_advertise_completed_r )
+  );
+
+  //! Delay counter - random value between 0-4 delay
+  KL_counter TMR_DELAY_COUNTER (
+    .clk_i         ( clk_i ),
+    .rst_n         ( rst_n ),
+    .start_i       ( start_tmr_delay_cnt_r ),
+    .stop_i        ( stop_tmr_delay_i ),
+    .delay_value_i ( delay_value_r ),
+    .completed_o   ( tmr_delay_completed_r )
+  );
+// --------------- MAIN LOGIC --------------- //
+
+  //! Free-running counter logic - value in between 0-4
+  always_ff @(posedge clk_i) begin : counter_logic
+    if (!rst_n) 
+      zero_four_sec_cnt_r <= 3'd0;
+    else begin
+      if (zero_four_sec_cnt_r == 3'd4)
+        zero_four_sec_cnt_r <= 3'd0;
+      else
+        zero_four_sec_cnt_r <= zero_four_sec_cnt_r + 3'd1;
+    end
+  end
+
+
+  //! Process for assigning the delay value for TMR_DELAY_COUNTER
+  always_ff @(posedge clk_i) begin : tmr_delay_process
+    if (!rst_n) begin
+      delay_value_r <= 3'd0;
+      start_tmr_delay_cnt_r <= 1'd0;
+    end
+    else begin
+      if (start_tmr_delay_i) begin
+        delay_value_r <= zero_four_sec_cnt_r;
+        start_tmr_delay_cnt_r <= 1'd1;
+      end 
+      else begin
+        start_tmr_delay_cnt_r <= 1'd0;
+      end
+    end
+  end
+
+  //! Process for checking whether grandmaster_id has changed
+  //! for the ATDECC Entity.
+  always_ff @(posedge clk_i) begin : grandmaster_control
+    if (!rst_n) begin
+      grandmaster_id_r <= 64'd0;
+      gm_change_r <= 1'd0;
+    end
+    else begin
+      grandmaster_id_r <= grandmaster_id_i;
+      if (grandmaster_id_r != grandmaster_id_i)
+        gm_change_r <= 1'd1;
+      else
+        gm_change_r <= 1'd0;
+    end
+  end
+
+  //! Process for handling the RCV_ADP_DISCOVER EVENT;
+  //! When the rcv_adp_discover input arrives, control the
+  //! rcvd_entity_info.entity_id whether it is ZERO or 
+  //! ATDECC Entity's Entity_ID(entity_id_i)
+  always_ff @(posedge clk_i) begin : rcv_adp_discover_event
+    if (!rst_n) begin
+      rcv_adp_discover_r <= 1'd0;
+      cntrller_state <= IDLE_S;
+    end
+    else begin
+      case (cntrller_state)
+
+        IDLE_S : begin
+          rcv_adp_discover_r <= 1'd0;
+          if (rcv_adp_discover_i)
+            cntrller_state <= CHECK_ENTITY_S;
+        end
+
+        CHECK_ENTITY_S : begin
+          if (rcv_entity_id_i == 64'd0 || rcv_entity_id_i == entity_id_i)
+            cntrller_state <= UPDATE_EVENT_S;
+          else
+            cntrller_state <= IDLE_S;
+        end
+
+        UPDATE_EVENT_S : begin
+          rcv_adp_discover_r <= 1'd1;
+          cntrller_state <= IDLE_S;
+        end
+      endcase
+    end
+  end
+
+endmodule
+
+`default_nettype wire
