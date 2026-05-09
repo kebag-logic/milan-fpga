d549fd71f207d7f1bf7de5699cb830b9f24d4cfe
max-pinaud <maximilien.pinaud@icloud.com>
2026-05-09 10:53:46 +0200

    [hdl/tb] First implementation of the buffered switch to be used as a configurable switch to address packets to various ports depending on the axis tdest value

---
+++ hdl/1722/buffered_switch.sv
@@ -0,0 +1,297 @@
+`default_nettype none
+module buffered_switch # (
+    parameter bit   IN_REG          = 1,                    //! Enable input stream registering
+    parameter bit   OUT_REG         = 1,                    //! Enable output stream registering
+    parameter int   FIFO_DEPTH      = 128,                  //! The depth in terms of samples of the FIFO
+    parameter int NUM_M_AXIS_IF = 3,
+    parameter int PORT_VALUES[NUM_M_AXIS_IF] = '{1, 2, 3}
+    )(
+    input wire              i_clk, i_resetn,                //! Clock and Reset Signals 
+    axi_stream_if.slave     s_axis,                         //! AXI4-Stream Slave interface
+    axi_stream_if.master    m_axis[NUM_M_AXIS_IF]              //! AXI4-Stream Master interface
+);
+
+localparam int TDATA_WIDTH          =    $bits(s_axis.tdata);    //! Measure of the axis data port width
+localparam int TUSER_WIDTH          =    $bits(s_axis.tuser);    //! Measure of the axis user port width
+localparam int TDEST_WIDTH          =    $bits(s_axis.tdest);    //! Measure of the axis destination port width
+localparam int TID_WIDTH            =    $bits(s_axis.tid);      //! Measure of the axis id width
+localparam int TSTRB_WIDTH          =    $bits(s_axis.tstrb);    //! Measure of the axis strobe width
+localparam int OUT_PIPELINE_LENGTH  =    2;                      //! Defines the output pipeline length for the skid buffer 
+
+initial begin 
+    //! Check for duplicate port values on instantiation of the module
+    for (int i = 0; i < NUM_M_AXIS_IF; i++) begin
+        for (int j = i + 1; j < NUM_M_AXIS_IF; j++) begin
+            if (PORT_VALUES[i] == PORT_VALUES[j]) begin
+                $fatal(1,
+                    "ERROR: PORT_VALUES has duplicate entries: [%0d]=%0d and [%0d]=%0d",
+                    i, PORT_VALUES[i], j, PORT_VALUES[j]);
+            end
+        end
+    end
+    //! Check that the number of axi interfaces is grater than 0
+    assert (NUM_M_AXIS_IF > 0) else  
+    $error("There should be at least on PORT value to instantiate one output port");
+    //! Check that fifo depth value is correct
+    assert (FIFO_DEPTH > 0 && (FIFO_DEPTH%16 == 0)) else 
+    $error("FIFO size should be larger than 0 and a power of 2");
+end 
+
+//! axis streaming struct used throughout the design
+typedef struct packed {
+  logic [TDATA_WIDTH-1:0]       tdata;
+  logic [(TDATA_WIDTH/8)-1:0]   tkeep;
+  logic [TDEST_WIDTH-1:0]       tdest;
+  logic [TUSER_WIDTH-1:0]       tuser;
+  logic [TID_WIDTH-1:0]         tid;
+  logic [TSTRB_WIDTH-1:0]       tstrb;
+  logic                         tvalid;
+  logic                         tlast;
+  logic                         tready;
+} axis_t;
+
+axis_t wi_fifo,wo_fifo; 
+logic fifo_ready;
+
+generate
+    if(|IN_REG)
+    begin
+        //! When the input registering is enable a skid buffer is generated
+        axis_t fifo_data, fifo_hold;
+        logic in_full;
+        always_ff@(posedge i_clk)
+        begin
+            if(~i_resetn)
+            begin
+                in_full         <= 'b0;
+            end
+            else
+            begin
+                fifo_data.tdata <= s_axis.tdata;
+                fifo_data.tkeep <= s_axis.tkeep;
+                fifo_data.tdest <= s_axis.tdest;
+                fifo_data.tuser <= s_axis.tuser;
+                fifo_data.tid   <= s_axis.tid;
+                fifo_data.tstrb <= s_axis.tstrb;
+                fifo_data.tvalid<= s_axis.tvalid;
+                fifo_data.tlast <= s_axis.tlast;
+                if(wi_fifo.tready)
+                begin
+                    in_full     <= 'b0;
+                end
+                else if (fifo_data.tvalid && ~wi_fifo.tready && ~in_full)
+                begin
+                    in_full     <= 1'b1;
+                    fifo_hold   <= fifo_data;
+                end
+            end
+        end
+        always_comb begin
+            wi_fifo        <= (in_full) ? fifo_hold : fifo_data;
+            wi_fifo.tready <= fifo_ready;
+        end
+        assign s_axis.tready = ~in_full;
+    end
+    else
+    begin
+        //! When the input registering is disabled connect to port directly
+        assign wi_fifo.tdata = s_axis.tdata;
+        assign wi_fifo.tkeep = s_axis.tkeep;
+        assign wi_fifo.tdest = s_axis.tdest;
+        assign wi_fifo.tuser = s_axis.tuser;
+        assign wi_fifo.tid   = s_axis.tid;
+        assign wi_fifo.tstrb = s_axis.tstrb;
+        assign wi_fifo.tvalid= s_axis.tvalid;
+        assign wi_fifo.tlast = s_axis.tlast;
+        assign wi_fifo.tready= fifo_ready;
+        assign s_axis.tready = wi_fifo.tready;
+    end
+endgenerate
+
+//! axi streaming fifo instantiation
+xpm_fifo_axis #(
+.CLOCKING_MODE("common_clock"),
+.FIFO_DEPTH(FIFO_DEPTH),
+.FIFO_MEMORY_TYPE("auto"),
+.PACKET_FIFO("false"),
+.TDATA_WIDTH(TDATA_WIDTH),
+.TUSER_WIDTH(TUSER_WIDTH),
+.TDEST_WIDTH(TDEST_WIDTH),
+.TID_WIDTH(TID_WIDTH)
+
+)
+xpm_fifo_axis_inst_A(
+.s_aclk(i_clk),
+.s_aresetn(i_resetn),
+
+.s_axis_tdata   (wi_fifo.tdata),
+.s_axis_tkeep   (wi_fifo.tkeep),
+.s_axis_tlast   (wi_fifo.tlast),
+.s_axis_tready  (fifo_ready),
+.s_axis_tvalid  (wi_fifo.tvalid),
+.s_axis_tdest   (wi_fifo.tdest),
+.s_axis_tuser   (wi_fifo.tuser),
+.s_axis_tid     (wi_fifo.tid),
+.s_axis_tstrb   (wi_fifo.tstrb),
+
+.m_axis_tdata   (wo_fifo.tdata),
+.m_axis_tkeep   (wo_fifo.tkeep),
+.m_axis_tlast   (wo_fifo.tlast),
+.m_axis_tready  (wo_fifo.tready),
+.m_axis_tvalid  (wo_fifo.tvalid),
+.m_axis_tdest   (wo_fifo.tdest),
+.m_axis_tuser   (wo_fifo.tuser),
+.m_axis_tid     (wo_fifo.tid),
+.m_axis_tstrb   (wo_fifo.tstrb)
+);
+
+//! Function used to verify if one hot encoding can be used on the output
+function automatic bit is_contiguous(input int arr[], input int n);
+    for (int i = 1; i < n; i++) begin
+        if (arr[i-1] != i)
+            return 0;
+    end
+    return 1;
+endfunction
+
+//! If data is not contiguous the user is warned
+initial begin
+    if (!is_contiguous(PORT_VALUES, NUM_M_AXIS_IF)) begin
+        $warning(1, "WARNING: PORT_VALUES are not contiguous. Cannot use shift-based one-hot decode.");
+    end
+end
+localparam bit USE_SHIFT_DECODE = is_contiguous(PORT_VALUES, NUM_M_AXIS_IF);
+
+//! Based on the tdest value a interface is selected via the sel variable
+logic [NUM_M_AXIS_IF:0] sel;
+logic [NUM_M_AXIS_IF-1:0] hold_sel, out_sel,valid_sel;
+assign valid_sel = sel[NUM_M_AXIS_IF:1] & {NUM_M_AXIS_IF{wo_fifo.tvalid}};
+generate
+    if(|USE_SHIFT_DECODE)
+    begin
+        //! one hot encoding to select the output port
+        assign sel = (1 << wo_fifo.tdest);
+    end
+    else
+    begin
+        //! comparator to select the output port (less performance)
+        for (genvar i = 0; i < NUM_M_AXIS_IF; i++) 
+        begin : GEN_DECODE
+            assign sel[i+1] = (wo_fifo.tdest == PORT_VALUES[i]);
+        end
+    end
+endgenerate
+
+//! skid buffer in order to register data and avoid a high fanout
+logic full, m_axis_tready;
+assign wo_fifo.tready = ~full;
+axis_t hold_data, r_data;
+always_ff@(posedge i_clk)
+begin
+    if(~i_resetn)
+    begin
+        full <= 'b0;
+    end
+    else
+    begin
+        r_data          <= (~full) ? wo_fifo : r_data;
+        out_sel         <= (~full) ? valid_sel : out_sel;
+        if (m_axis_tready)
+        begin
+            full        <= 'b0;
+        end
+        else if (r_data.tvalid && ~m_axis_tready && ~full)
+        begin
+            full        <= 1'b1;
+            hold_data   <= r_data;
+            hold_sel    <= out_sel;
+        end
+    end
+end
+
+//! Combinatorial tready check
+logic [NUM_M_AXIS_IF-1:0] tready_vec, out_full;
+axis_t [NUM_M_AXIS_IF-1:0] r_axis;
+generate 
+    if(|OUT_REG)
+    begin
+        for (genvar i = 0; i < NUM_M_AXIS_IF; i++) begin
+            assign tready_vec[i] = (full) ? 
+                                   ~out_full[i] && hold_sel[i] : 
+                                   ~out_full[i] && out_sel[i];
+        end
+    end
+    else
+    begin
+        for (genvar i = 0; i < NUM_M_AXIS_IF; i++) begin
+            assign tready_vec[i] = (full) ? 
+                                   r_axis[i].tready && hold_sel[i] : 
+                                   r_axis[i].tready && out_sel[i];
+        end
+    end
+endgenerate
+assign m_axis_tready = |tready_vec;
+
+generate 
+    for (genvar i = 0; i < NUM_M_AXIS_IF; i++) begin : GEN_PORTS
+        always_comb begin
+            r_axis[i].tdata  <= (full) ? hold_data.tdata : r_data.tdata;
+            r_axis[i].tuser  <= (full) ? hold_data.tuser : r_data.tuser;
+            r_axis[i].tlast  <= (full) ? hold_data.tlast : r_data.tlast;
+            r_axis[i].tdest  <= (full) ? hold_data.tdest : r_data.tdest;
+            r_axis[i].tid    <= (full) ? hold_data.tid   : r_data.tid;
+            r_axis[i].tstrb  <= (full) ? hold_data.tstrb : r_data.tstrb;
+            r_axis[i].tvalid <= (full) ? hold_sel[i]     : out_sel[i];
+            r_axis[i].tkeep  <= (full) ? hold_data.tkeep : r_data.tkeep;
+        end
+        if(|OUT_REG)
+        begin
+            //! When the output registering is enable a skid buffer is generated for each interface
+            axis_t [NUM_M_AXIS_IF-1:0] out_hold, out_data;
+            always_ff@(posedge i_clk)
+            begin
+                if(~i_resetn)
+                begin
+                    out_full[i]         <= 'b0;
+                end
+                else
+                begin
+                    out_data[i] <= (~out_full[i])? r_axis[i] : out_data[i]; 
+                    if(r_axis[i].tready)
+                    begin
+                        out_full[i]     <= 'b0;
+                    end
+                    else if (out_data[i].tvalid && ~r_axis[i].tready && ~out_full[i])
+                    begin
+                        out_full[i]     <= 1'b1;
+                        out_hold[i]     <= out_data[i];
+                    end
+                end
+            end
+            assign m_axis[i].tdata  = (out_full[i]) ? out_hold[i].tdata  : out_data[i].tdata;
+            assign m_axis[i].tuser  = (out_full[i]) ? out_hold[i].tuser  : out_data[i].tuser; 
+            assign m_axis[i].tlast  = (out_full[i]) ? out_hold[i].tlast  : out_data[i].tlast; 
+            assign m_axis[i].tdest  = (out_full[i]) ? out_hold[i].tdest  : out_data[i].tdest; 
+            assign m_axis[i].tid    = (out_full[i]) ? out_hold[i].tid    : out_data[i].tid;   
+            assign m_axis[i].tstrb  = (out_full[i]) ? out_hold[i].tstrb  : out_data[i].tstrb; 
+            assign m_axis[i].tvalid = (out_full[i]) ? out_hold[i].tvalid : out_data[i].tvalid;
+            assign m_axis[i].tkeep  = (m_axis[i].tvalid) ? (out_full[i]) ? out_hold[i].tkeep  : out_data[i].tkeep : 'b0;
+            assign r_axis[i].tready = m_axis[i].tready;
+        end
+        else
+        begin
+            assign m_axis[i].tdata  = r_axis[i].tdata;
+            assign m_axis[i].tuser  = r_axis[i].tuser; 
+            assign m_axis[i].tlast  = r_axis[i].tlast; 
+            assign m_axis[i].tdest  = r_axis[i].tdest; 
+            assign m_axis[i].tid    = r_axis[i].tid;   
+            assign m_axis[i].tstrb  = r_axis[i].tstrb; 
+            assign m_axis[i].tvalid = r_axis[i].tvalid;
+            assign m_axis[i].tkeep  = (m_axis[i].tvalid) ? r_axis[i].tkeep : 'b0;
+            assign r_axis[i].tready = m_axis[i].tready;
+        end
+    end
+endgenerate
+
+endmodule
+`default_nettype wire
---
+++ tb/utests/1722/buffered_switch/tb_top.sv
@@ -0,0 +1,227 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Cemal Dogan <cemal.dogan@kebag-logic.com>
+ *
+ * SPDX-License-Identifier: CERN-OHL-W-2.0
+ */
+
+module tb_top();
+
+  `include "avtp_packet_gen_pkg.svh"
+  `include "axi_stream_driver.svh"
+
+  avtp_control_subtype avtp_control_pkt;
+  avtp_stream_subtype avtp_stream_pkt;
+  avtp_alter_subtype avtp_alter_pkt;
+  avtp_random_subtype avtp_random_pkt;
+  avtp_common_hdr_t avtp_common_hdr;
+  logic [7:0] pkt[$];
+
+  parameter TDATA_WIDTH_P = 128;
+  parameter TDEST_WIDTH_P = 2;
+  parameter TUSER_WIDTH_P = 8;
+  parameter T = 10;
+  parameter PIPELINE_DELAY = 1;
+  bit clk;
+  bit rst_n;
+
+  // DUT counters
+  int number_of_supported_control = 0;
+  int number_of_supported_stream = 0;
+  int number_of_supported_alter = 0;
+  int number_of_not_supported = 0;
+  int error_supported_control = 0;
+  int error_supported_stream = 0;
+  int error_supported_alter = 0;
+  int error_not_supported = 0;
+
+  int sent_supported_control = 0;
+  int sent_supported_stream = 0;
+  int sent_supported_alter = 0;
+  int sent_not_supported = 0;
+
+  axi_stream_if #(.TDATA_WIDTH_P(TDATA_WIDTH_P), .TDEST_WIDTH_P(TDEST_WIDTH_P),.TUSER_WIDTH_P(TUSER_WIDTH_P)) m_axis_top_3(clk, rst_n);
+  axi_stream_if #(.TDATA_WIDTH_P(TDATA_WIDTH_P), .TDEST_WIDTH_P(TDEST_WIDTH_P),.TUSER_WIDTH_P(TUSER_WIDTH_P)) m_axis_top_2(clk, rst_n);
+  axi_stream_if #(.TDATA_WIDTH_P(TDATA_WIDTH_P), .TDEST_WIDTH_P(TDEST_WIDTH_P),.TUSER_WIDTH_P(TUSER_WIDTH_P)) m_axis_top_1(clk, rst_n);
+  axi_stream_if #(.TDATA_WIDTH_P(TDATA_WIDTH_P), .TDEST_WIDTH_P(TDEST_WIDTH_P),.TUSER_WIDTH_P(TUSER_WIDTH_P)) m_axis_top_0(clk, rst_n);
+  
+  axi_stream_if #(.TDATA_WIDTH_P(TDATA_WIDTH_P), .TDEST_WIDTH_P(TDEST_WIDTH_P),.TUSER_WIDTH_P(TUSER_WIDTH_P)) s_axis_top(clk, rst_n);
+
+  axi_stream_driver #(.CLK_PERIOD_P(T), .TDATA_WIDTH_P(TDATA_WIDTH_P), .TDEST_WIDTH_P(TDEST_WIDTH_P),.TUSER_WIDTH_P(TUSER_WIDTH_P)) axis_driver;
+
+  buffered_switch
+  #(
+    .IN_REG(1),
+    .OUT_REG(1),
+    .NUM_M_AXIS_IF(4),
+    .PORT_VALUES({0,1,2,3}),
+    .FIFO_DEPTH(128)
+  ) DUT (
+    .i_clk(clk),
+    .i_resetn(rst_n),
+    .s_axis(s_axis_top),
+    .m_axis({m_axis_top_0,m_axis_top_1,m_axis_top_2,m_axis_top_3})
+  );
+
+  always #(T/2) clk = ~clk;
+
+  task reset_dut;
+    #100;
+    $display("[INFO][TOP] : Reseting the DUT");
+    rst_n = 1'd1;
+  endtask
+
+  task drive_random_pkt;
+    avtp_common_hdr = avtp_random_pkt.subtype_header_gen();
+    avtp_random_pkt.avtp_packet_gen(pkt);
+    // For now fill the rest with random data
+    for (int i = 0; i < 18; i++) begin
+      pkt.push_back($urandom_range(0,255));
+    end
+    #150ns;
+    axis_driver.drive_axi_stream(pkt);
+  endtask
+
+  task report_summary;
+    $display("[INFO][TOP] : Reporting Summary from the AVTP Random Generator Class");
+    $display("---------------------------------------------------------------------");
+    $display("[INFO][TOP] : Total # of AVTP Packet Generated is              %d", avtp_random_pkt.total_count);
+    //$display("[INFO][TOP] : # of Control Packet Generated %d", avtp_random_pkt.control_count);
+    //$display("[INFO][TOP] : # of Stream Packet Generated %d", avtp_random_pkt.stream_count);
+    //$display("[INFO][TOP] : # of Alternative Packet Generated %d", avtp_random_pkt.alter_count);
+    //$display("[INFO][TOP] : # of Non-AVTP Packet Generated %d", (avtp_random_pkt.total_count - (avtp_random_pkt.control_count + avtp_random_pkt.stream_count + avtp_random_pkt.alter_count)));
+    $display("");
+    $display("[INFO][TOP] : # of Control Packet Generated                    %d", sent_supported_control);
+    $display("[INFO][TOP] : # of Stream Packet Generated                     %d", sent_supported_stream);
+    $display("[INFO][TOP] : # of Alternative Packet Generated                %d", sent_supported_alter);
+    $display("[INFO][TOP] : # of Non-AVTP Packet Generated                   %d", sent_not_supported);
+    $display("");
+    $display("[INFO][TOP] : Reporting Summary from the DUT");
+    $display("---------------------------------------------------------------------");
+    $display("[INFO][TOP] : # of Control Packet Parsed                       %d ",number_of_supported_control);
+    $display("[INFO][TOP] : # of Stream Packet Parsed                        %d ", number_of_supported_stream);
+    $display("[INFO][TOP] : # of Alternative Packet Parsed                   %d ", number_of_supported_alter);
+    $display("[INFO][TOP] : # of Total Not supported Packet Parsed           %d ", number_of_not_supported);
+    $display("[INFO][TOP] : # of Errors on Control Packet Parsed             %d ",error_supported_control);
+    $display("[INFO][TOP] : # of Errors on Stream Packet Parsed              %d ", error_supported_stream);
+    $display("[INFO][TOP] : # of Errors on Alternative Packet Parsed         %d ", error_supported_alter);
+    $display("[INFO][TOP] : # of Errors on Total Not supported Packet Parsed %d ", error_not_supported);
+  endtask
+
+  task check_total_parse_pkt;
+    if (avtp_random_pkt.total_count == (number_of_not_supported + number_of_supported_control + number_of_supported_stream + number_of_supported_alter))
+      $display("[INFO][TOP] : All Generated Pkts Parsed Correctly. ");
+    else 
+      $fatal(1, "[FATAL][TOP] : # number of total generated pkts is not equal to # of total parsed");
+  endtask
+
+
+  initial begin
+    m_axis_top_0.tready = 1;
+    m_axis_top_1.tready = 1;
+    m_axis_top_2.tready = 1;
+    m_axis_top_3.tready = 1;
+    axis_driver = new();
+    avtp_random_pkt = new();
+    axis_driver.axis_if = s_axis_top; // Bind the interfaces
+    $display("[INFO][TOP] : ---------------------TEST CASE-I-------------------------------");
+    $display("[INFO][TOP] : Randomly Generating the AVTP Packets");
+    reset_dut();
+    for (int i = 0; i < 10000; i++) begin
+      //s_axis_top.tdest = $urandom_range(1,3);
+      drive_random_pkt();
+    end
+    #100ns;
+    report_summary();
+    #100ns;
+    check_total_parse_pkt();
+    $finish;
+  end
+  
+  always@(posedge clk)
+  begin
+    m_axis_top_0.tready <= $urandom_range(0,1);
+    m_axis_top_1.tready <= $urandom_range(0,1);
+    m_axis_top_2.tready <= $urandom_range(0,1);
+    m_axis_top_3.tready <= $urandom_range(0,1);
+  end
+
+  logic r_parser_state,parser_state;
+  localparam IDLE_S = 0;
+  localparam WAIT_S = 1;
+
+  always_comb begin : subtype_save_logic
+      parser_state <= r_parser_state;
+      case (r_parser_state)
+        IDLE_S : begin
+          if (s_axis_top.tvalid) begin
+            s_axis_top.tuser <= s_axis_top.tdata[47:40];
+            parser_state     <= WAIT_S;
+          end
+        end
+
+        WAIT_S : begin
+          if (s_axis_top.tready & s_axis_top.tvalid & s_axis_top.tlast) parser_state <= IDLE_S;
+        end
+
+        default : begin
+          parser_state <= IDLE_S;
+        end
+      endcase
+  end : subtype_save_logic
+
+  always@(posedge clk)
+  begin
+      if(~rst_n)
+      begin
+         r_parser_state <= IDLE_S;
+      end
+      else
+      begin
+         r_parser_state <= parser_state;
+      end
+  end
+
+  always_comb begin : tdest_assign
+   case (s_axis_top.tuser)
+      ADP, AECP, ACMP, MAAP: s_axis_top.tdest = 'h0;
+      IIDC, MMA_STREAM, AAF: s_axis_top.tdest = 'h1;
+      CRF : s_axis_top.tdest                  = 'h2;
+      default : s_axis_top.tdest              = 'h3;
+    endcase
+  end : tdest_assign
+
+  // Check whether the received packet control|stream|alter|not supported
+  logic packet_valid[3:0];
+
+  assign packet_valid[0] =  m_axis_top_0.tkeep == 'hF800 && m_axis_top_0.tready;
+  assign packet_valid[1] =  m_axis_top_1.tkeep == 'hF800 && m_axis_top_1.tready;
+  assign packet_valid[2] =  m_axis_top_2.tkeep == 'hF800 && m_axis_top_2.tready;
+  assign packet_valid[3] =  m_axis_top_3.tkeep == 'hF800 && m_axis_top_3.tready;
+  
+  always @(posedge clk) begin
+    // Check the tdest w.r.t to tkeep since we do not drive the tvalid and tlast
+    // if the packet is not supported
+
+    if (packet_valid[0] && m_axis_top_0.tdest == 0) number_of_supported_control++;
+    else if (packet_valid[0]) error_supported_control++;
+    if (packet_valid[1] && m_axis_top_1.tdest == 1) number_of_supported_stream++;
+    else if (packet_valid[1]) error_supported_stream++;
+    if (packet_valid[2] && m_axis_top_2.tdest == 2) number_of_supported_alter++;
+    else if (packet_valid[2]) error_supported_alter++;
+    if (packet_valid[3] && m_axis_top_3.tdest == 3) number_of_not_supported++;
+    else if (packet_valid[3]) error_not_supported++;
+  end
+
+  always @(posedge clk) begin
+    // Check the tdest w.r.t to tkeep since we do not drive the tvalid and tlast
+    // if the packet is not supported
+    if(s_axis_top.tready)
+    begin
+        if (s_axis_top.tkeep == 'hF800 && s_axis_top.tdest == 0) sent_supported_control++;
+        if (s_axis_top.tkeep == 'hF800 && s_axis_top.tdest == 1) sent_supported_stream++;
+        if (s_axis_top.tkeep == 'hF800 && s_axis_top.tdest == 2) sent_supported_alter++;
+        if (s_axis_top.tkeep == 'hF800 && s_axis_top.tdest == 3) sent_not_supported++;
+    end
+  end
+
+endmodule
